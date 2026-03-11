90d14d02830323c4ccc3eb0713b07df54cb54dfb
Sorin C. <csorin.dev@gmail.com>
2026-03-11 13:12:59 +0100

    fix: scroll to top when changing page

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,25 @@ import NavidadPage from './pages/NavidadPage'
 import ContactoPage from './pages/ContactoPage'
 import AvisoLegalPage from './pages/AvisoLegalPage'
 import PrivacidadPage from './pages/PrivacidadPage'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/embarazo" element={<EmbarazoPage />} />
-        <Route path="/bebes" element={<BebesPage />} />
-        <Route path="/comuniones" element={<ComunionesPage />} />
-        <Route path="/navidad" element={<NavidadPage />} />
-        <Route path="/contacto" element={<ContactoPage />} />
-        <Route path="/aviso-legal" element={<AvisoLegalPage />} />
-        <Route path="/politica-privacidad" element={<PrivacidadPage />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/embarazo" element={<EmbarazoPage />} />
+          <Route path="/bebes" element={<BebesPage />} />
+          <Route path="/comuniones" element={<ComunionesPage />} />
+          <Route path="/navidad" element={<NavidadPage />} />
+          <Route path="/contacto" element={<ContactoPage />} />
+          <Route path="/aviso-legal" element={<AvisoLegalPage />} />
+          <Route path="/politica-privacidad" element={<PrivacidadPage />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
